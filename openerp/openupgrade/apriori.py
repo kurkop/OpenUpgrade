99456401210b976c2320827394a2ec71014acf29
Juan Pablo Arias <jpa@devco.co>
2015-09-18 09:47:04 -0500

    [IMP] update apriory.py with default values}

--- openerp/openupgrade/apriori.py
+++ openerp/openupgrade/apriori.py
@@ -5,29 +5,24 @@ to help the matching process
 from openerp.openupgrade import openupgrade
 
 renamed_modules = {
-    # 'base_calendar': 'calendar',
-    # 'mrp_jit': 'procurement_jit',
+    'base_calendar': 'calendar',
+    'mrp_jit': 'procurement_jit',
     # # OCA/account-invoicing
-    # 'invoice_validation_wkfl': 'account_invoice_validation_workflow',
-    # 'account_invoice_zero': 'account_invoice_zero_autopay',
+    'invoice_validation_wkfl': 'account_invoice_validation_workflow',
+    'account_invoice_zero': 'account_invoice_zero_autopay',
     # # OCA/server-tools
-    # 'audittrail': 'auditlog',
+    'audittrail': 'auditlog',
     # # OCA/bank-statement-import
-    # 'account_banking': 'account_bank_statement_import',
-    # 'account_banking_camt': 'bank_statement_parse_camt',
-    # 'account_banking_nl_ing_mt940': 'bank_statement_parse_nl_ing_mt940',
-    # 'account_banking_nl_rabo_mt940': 'bank_statement_parse_nl_rabo_mt940',
-    'l10n_co_base': 'l10n_co_geo',
-    'l10n_co_razonsocial': 'l10n_co_terceros',
-    #'l10n_co_dian_auth': 'l10n_co_terceros',
-    'l10n_co_retenciones': 'l10n_co_core',
+    'account_banking': 'account_bank_statement_import',
+    'account_banking_camt': 'bank_statement_parse_camt',
+    'account_banking_nl_ing_mt940': 'bank_statement_parse_nl_ing_mt940',
+    'account_banking_nl_rabo_mt940': 'bank_statement_parse_nl_rabo_mt940',
 }
 
 renamed_models = {
 }
 
 module_names_merge = {
-    'l10n_co_dian_auth': 'l10n_co_terceros',
 }
 
 def migrate_apriori(cr):
